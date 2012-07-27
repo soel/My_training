3dfb7dc91402cba2fafaa9eb09ba6dd11979ad9e
Akihiro KAMBARA <kanbara@www16290ue.sakura.ne.jp>
2012-07-27 10:17:22 +0900

    アルゴリズム変更

--- redunmail.py
+++ redunmail.py
@@ -14,9 +14,10 @@ if argc != 2: #引数が正しいか確認　2だと正しい
 else:
     f = open(argvs[1]) #ファイルオープン
    
-    sum_mail = [] #リストの作成   
+    sum_mail = [] #リストの作成  
+    sumset_mail = set() #set型の作成
     for line in f.readlines(): #1行ずつ読み込む
-
+        line = line.strip() #改行を取り除く
         mail = re.findall("([\w\.-]+@[\w\.-]+)", line)
         #メールの正規表現により抽出
         num = re.search("[0-9]{6}", line)
@@ -29,15 +30,18 @@ else:
         for va_mail in set_mail: #セット型の展開
             list_mail.append(va_mail) #リスト型へ変換し代入
         list_mail.reverse() #読み取った順番になっているのでもとに戻す
-        sum_mail.append(num.group(0) + ',' +  string.join(list_mail, ';'))
-        #ID+メールアドレスの結合をリスト型で追加
+        sumset_mail.add(num.group(0) + ',' +  string.join(list_mail, ';'))
+        #ID+メールアドレスの結合をセット型で追加　ID重複を排除
 
-    f.close() #ファイル閉じる
+    f.close() #ファイルを閉じる
+    sumlist_mail = [] #リスト型の作成
+    for sum_mail in sumset_mail: #set型をリスト型に展開
+        sumlist_mail.append(sum_mail)
 
-    sum_mail.sort() #内容をソートして順番をID順にする
+    sumlist_mail.sort() #内容をソートして順番をID順にする
     
     f = open('custmail.txt', 'w') #custmail.txt として出力
-    for write_mail in sum_mail: #リスト型 sum_mail を展開
+    for write_mail in sumlist_mail: #リスト型 sumlist_mail を展開
         f.write(write_mail+'\n') #展開したデータを書き込む
 
-    f.close()
+    f.close() #ファイルを閉じる
