2480a767ca5a910f7468690ae22b839c976f0c71
Akihiro KAMBARA <kanbara@www16290ue.sakura.ne.jp>
2012-09-25 15:04:49 +0900

    revise

--- sqltime.py
+++ sqltime.py
@@ -7,8 +7,8 @@ import datetime
 d = datetime.datetime.today()
 t = "%s.%s.%s.%s.%s.%s" % (d.year, d.month, d.day, d.hour, d.minute, d.second)
 
-con = MySQLdb.connect(host = 'localhost', db = 'time', user = 'root', passwd =
-        'P@ssw0rd', charset = 'utf8')
+con = MySQLdb.connect(host = 'localhost', db = 'time', user = 'root',
+        passwd = 'P@ssw0rd', charset = 'utf8')
 cur = con.cursor(MySQLdb.cursors.DictCursor)
 
 cur.execute("""INSERT INTO time VALUES('%s')""" % t)
