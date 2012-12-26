974353cf08005bd040b43cd90f403735924236a0
Akihiro KAMBARA <kanbara@www16290ue.sakura.ne.jp>
2012-12-26 09:44:17 +0900

    delete sudo

--- shell/named_failed_to_connect.sh
+++ shell/named_failed_to_connect.sh
@@ -6,8 +6,7 @@ LOGFILE=/var/log/named/log.0
 NOTIFY="," #mail address
 BODY="DNSサーバーのログファイル内でエラーを確認しましたので調査してください"
 
-sudo cat ${LOGFILE} | grep "failed to connect" > ${TMP}/failed_to_connect.txt
-#パーミッションの問題で sudo にて実施
+cat ${LOGFILE} | grep "failed to connect" > ${TMP}/failed_to_connect.txt
 
 if [ -s ${TMP}/failed_to_connect.txt ]; then
   sed -i '1s/^/'${BODY}'\n\n/' ${TMP}/failed_to_connect.txt #先頭に文章を追加
